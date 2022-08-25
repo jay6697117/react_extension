44e9d73bf0c29fe8ee0764f9cf6c7108c4cc3c59
steve zhang <871506263@qq.com>
2022-08-26 00:06:49 +0800

    提交代码于: 2022-08-26 00:06:49

--- src/components/7_renderProps/index.jsx
+++ src/components/7_renderProps/index.jsx
@@ -7,8 +7,12 @@ export default class Parent extends Component {
     return (
       <div className='parent'>
         <h3>我是Parent组件</h3>
-        <A render={name => <C name={name} />} />
-        <A render={name => <B name={name} />} />
+        {/* render props */}
+        {/* <A render={name => <C name={name} />} /> */}
+        {/* <A render={name => <B name={name} />} /> */}
+        {/* children props */}
+        <A>{name => <C name={name} />}</A>
+        <A>{name => <B name={name} />}</A>
       </div>
     );
   }
@@ -17,13 +21,16 @@ export default class Parent extends Component {
 class A extends Component {
   state = { name: 'tom' };
   render() {
-    console.log(this.props);
+    console.log('render A:',this.props);
     const { name } = this.state;
     return (
       <div className='a'>
         <h3>我是A组件 - {Math.floor(Math.random() * 1000000000)}</h3>
         <hr />
-        {this.props.render(name)}
+        {/* render props 预留位置 */}
+        {/* {this.props.render(name)} */}
+        {/* children props 预留位置 */}
+        {this.props.children(name)}
       </div>
     );
   }
